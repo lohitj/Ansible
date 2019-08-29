872137765aa0575a4645abae94decb0ad44fed64
lohitj <39816251+lohitj@users.noreply.github.com>
2019-08-29 07:07:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 DIRECTORY="/usr/bin/ansible"
 if [ ! -d "$DIRECTORY" ]; then
-        sudo apt update
-        sudo apt install software-properties-common
+        sudo apt -y update
+        sudo apt -y install software-properties-common
         sudo apt-add-repository --yes --update ppa:ansible/ansible
-        sudo apt install ansible
-	sudo apt install python
+        sudo apt -y install ansible
+	sudo apt -y install python
 fi
-ansible-playbook test-playbook.yml
+sudo ansible-playbook test-playbook.yml
